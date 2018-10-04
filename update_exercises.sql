6954d77f8eca67736d824192db0a3146a855fd06
brittanyrussell <brittanyrussell11032gmail.com>
2018-10-04 10:59:04 -0500

    3rd section in basic statements exercise completed part of 4th section

---
+++ update_exercises.sql
@@ -0,0 +1,18 @@
+SELECT album_name FROM albums;
+UPDATE albums
+SET sales = sales * 10;
+SELECT * FROM albums;
+
+SELECT album_name FROM albums
+WHERE release_date < '1980';
+UPDATE albums
+SET release_date = '1800'
+WHERE release_date < '1980';
+SELECT * FROM albums;
+
+SELECT album_name FROM albums
+WHERE artist_name='Michael Jackson';
+UPDATE albums
+SET artist_name = 'Peter Jackson'
+WHERE artist_name = 'Michael Jackson';
+SELECT * FROM albums;
