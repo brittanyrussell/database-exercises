180f5bc5bcf3e2b34fea17e23091ed6027cce4aa
brittanyrussell <brittanyrussell11032gmail.com>
2018-10-05 09:39:07 -0500

    limit exercise

---
+++ limit_exercises.sql
@@ -0,0 +1,18 @@
+USE employees;
+
+SELECT DISTINCT last_name
+FROM employees
+WHERE last_name LIKE 'Z%'
+ORDER BY last_name DESC
+ LIMIT 10;
+
+SELECT first_name, last_name
+FROM employees
+WHERE hire_date BETWEEN '1990-01-01' AND '1999-12-31'
+AND birth_date LIKE '%-12-25'
+ORDER BY birth_date ASC, hire_date DESC
+LIMIT 5 OFFSET 45 ;
+
+-- SELECT first_name, last_name
+-- FROM employees
+-- LIMIT 5 OFFSET 9;
